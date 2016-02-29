b4754f47ae51a6ae3d383a0f9e91580bc6dcd59a
aequanimitas <htavenido@gmail.com>
2016-02-29 16:58:27 +0800

    check if instance instead of length

--- test/containers/index.js
+++ test/containers/index.js
@@ -3,3 +3,13 @@ import expect from 'expect';
 import TestUtils from 'react-addons-test-utils';
 
 import App from '../../client/containers/App';
+
+describe('Single component', () => {
+
+  it('should contain 2 buttons', () => {
+    let renderer = TestUtils.createRenderer();
+    renderer.render(<App />);
+    let output = renderer.getRenderOutput();
+    expect(Array.isArray(output.props.children.length)).toBe(true);
+  });
+});
